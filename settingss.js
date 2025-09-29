1289c53dc55f833aa8e369295f738edb0d5366f3
EMPEROR-DAVE <discordbotz526@gmail.com>
2025-09-30 00:00:38 +0300

    Create settingss.js

---
+++ settingss.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'dave-md',
+  author: 'dave',
+  botName: "vampire md",
+  botOwner: 'Supreme', // Your name
+  ownerNumber: '254111687009', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.4",
+};
+
+module.exports = settings;
